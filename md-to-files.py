e7a054619b2aecef735a72b52734e28b5d388621
Åsmund Stavdahl <asmund.stavdahl@gmail.com>
2024-05-30 23:36:07 +0200

    feat: support text between filename header and its code block

--- md-to-files.py
+++ md-to-files.py
@@ -4,9 +4,10 @@ import re
 import os
 import sys
 
+
 def parse_markdown_and_save_files(markdown_content):
     # Regular expression to match code blocks and file names
-    pattern = re.compile(r'#### File: `(.*?)`\n```(.*?)\n(.*?)\n```', re.DOTALL)
+    pattern = re.compile(r"#### File: `(.*?)`\n.*?```(.*?)\n(.*?)\n```", re.DOTALL)
 
     matches = pattern.findall(markdown_content)
     for match in matches:
@@ -14,14 +15,19 @@ def parse_markdown_and_save_files(markdown_content):
         code_language = match[1].strip()
         code_content = match[2].strip()
 
+        print(f"Creating {file_path}... ", end="")
+
         # Create the directory if it doesn't exist
-        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        dir = os.path.dirname(file_path)
+        if dir != "":
+            os.makedirs(os.path.dirname(file_path), exist_ok=True)
 
         # Write the code content to the file
-        with open(file_path, 'w') as file:
+        with open(file_path, "w") as file:
             file.write(code_content)
 
-        print(f"Created file: {file_path}")
+        print("done")
+
 
 # Read the markdown content from STDIN
 markdown_content = sys.stdin.read()
